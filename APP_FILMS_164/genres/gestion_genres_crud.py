cd0ac2b64745040942fc55fa568907e9536e61a8
marcosit0 <marcosrb2001@gmail.com>
2023-06-04 17:22:13 +0200

    mon premier commit

--- APP_FILMS_164/genres/gestion_genres_crud.py
+++ APP_FILMS_164/genres/gestion_genres_crud.py
@@ -34,9 +34,8 @@ def genres_afficher(order_by, id_genre_sel):
         try:
             with DBconnection() as mc_afficher:
                 if order_by == "ASC" and id_genre_sel == 0:
-                    strsql_genres_afficher = """SELECT id_categorie, nom_categorie, montant_reel 
-                                                FROM t_categorie 
-                                                INNER JOIN t_pers_depense_reel_categorie t_pers_depense_reel_categorie ON t_pers_depense_reel_categorie.id_pers_dep_reel_cat = t_categorie.id_categorie"""
+                    strsql_genres_afficher = """SELECT id_categorie, nom_categorie, date_ins_categorie
+                                                FROM t_categorie"""
                     mc_afficher.execute(strsql_genres_afficher)
                 elif order_by == "ASC":
                     # C'EST LA QUE VOUS ALLEZ DEVOIR PLACER VOTRE PROPRE LOGIQUE MySql
@@ -45,15 +44,14 @@ def genres_afficher(order_by, id_genre_sel):
                     # donc, je précise les champs à afficher
                     # Constitution d'un dictionnaire pour associer l'id du genre sélectionné avec un nom de variable
                     valeur_id_genre_selected_dictionnaire = {"value_id_genre_selected": id_genre_sel}
-                    strsql_genres_afficher = """SELECT id_categorie, nom_categorie, montant_reel 
-                                                FROM t_categorie 
-                                                INNER JOIN t_pers_depense_reel_categorie t_pers_depense_reel_categorie ON t_pers_depense_reel_categorie.id_pers_dep_reel_cat = t_categorie.id_categorie"""
+                    strsql_genres_afficher = """SELECT id_categorie, nom_categorie, date_ins_categorie
+                                                FROM t_categorie"""
 
                     mc_afficher.execute(strsql_genres_afficher, valeur_id_genre_selected_dictionnaire)
                 else:
-                    strsql_genres_afficher = """SELECT id_categorie, nom_categorie, montant_reel 
-                                                FROM t_categorie 
-                                                INNER JOIN t_pers_depense_reel_categorie t_pers_depense_reel_categorie ON t_pers_depense_reel_categorie.id_pers_dep_reel_cat = t_categorie.id_categorie"""
+                    strsql_genres_afficher = """SELECT id_categorie, nom_categorie, date_ins_categorie
+                                                FROM t_categorie"""
+
 
                     mc_afficher.execute(strsql_genres_afficher)
 
@@ -112,7 +110,7 @@ def genres_ajouter_wtf():
                 valeurs_insertion_dictionnaire = {"value_intitule_genre": name_genre}
                 print("valeurs_insertion_dictionnaire ", valeurs_insertion_dictionnaire)
 
-                strsql_insert_genre = """INSERT INTO t_categorie (id_categorie,nom_categorie) VALUES (NULL,%(value_intitule_genre)s) """
+                strsql_insert_genre = """INSERT INTO t_categorie (id_categorie,nom_categorie,date_ins_categorie) VALUES (NULL,%(value_intitule_genre)s) """
                 with DBconnection() as mconn_bd:
                     mconn_bd.execute(strsql_insert_genre, valeurs_insertion_dictionnaire)
 
@@ -168,12 +166,12 @@ def genre_update_wtf():
 
             valeur_update_dictionnaire = {"value_id_categorie": id_genre_update,
                                           "value_name_categorie": name_genre_update,
-                                          "value_date_ins_essai": date_genre_essai
+                                          "value_date_ins_categorie": date_genre_essai
                                           }
             print("valeur_update_dictionnaire ", valeur_update_dictionnaire)
 
             str_sql_update_intitulegenre = """UPDATE t_categorie SET nom_categorie = %(value_name_genre)s, 
-            date_ins_genre = %(value_date_genre_essai)s WHERE id_categorie = %(value_id_genre)s """
+            date_ins_categorie = %(value_date_genre_essai)s WHERE id_categorie = %(value_id_genre)s """
             with DBconnection() as mconn_bd:
                 mconn_bd.execute(str_sql_update_intitulegenre, valeur_update_dictionnaire)
 
@@ -185,7 +183,7 @@ def genre_update_wtf():
             return redirect(url_for('genres_afficher', order_by="ASC", id_genre_sel=id_genre_update))
         elif request.method == "GET":
             # Opération sur la BD pour récupérer "id_genre" et "intitule_genre" de la "t_genre"
-            str_sql_id_genre = "SELECT id_categorie, nom_categorie FROM t_categorie " \
+            str_sql_id_genre = "SELECT id_categorie, nom_categorie, date_ins_categorie FROM t_categorie " \
                                "WHERE id_categorie = %(value_id_genre)s"
             valeur_select_dictionnaire = {"value_id_genre": id_genre_update}
             with DBconnection() as mybd_conn:
@@ -193,11 +191,11 @@ def genre_update_wtf():
             # Une seule valeur est suffisante "fetchone()", vu qu'il n'y a qu'un seul champ "nom genre" pour l'UPDATE
             data_nom_genre = mybd_conn.fetchone()
             print("data_nom_genre ", data_nom_genre, " type ", type(data_nom_genre), " genre ",
-                  data_nom_genre["intitule_genre"])
+                  data_nom_genre["nom_categorie"])
 
             # Afficher la valeur sélectionnée dans les champs du formulaire "personne_update_wtf.html"
             form_update.nom_genre_update_wtf.data = data_nom_genre["nom_categorie"]
-            # form_update.date_genre_wtf_essai.data = data_nom_genre["date_ins_genre"]
+            form_update.date_genre_wtf_essai.data = data_nom_genre["date_ins_categorie"]
 
     except Exception as Exception_genre_update_wtf:
         raise ExceptionGenreUpdateWtf(f"fichier : {Path(__file__).name}  ;  "
